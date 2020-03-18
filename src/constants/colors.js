5300399a2020d1e9e3d8298c9ce9b43c36c35c2b
leire <leirericop@gmail.com>
2020-03-18 13:30:55 +0100

    feat(constants): add palette colors

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,4 +1,25 @@
 // chicken logo
-export const yellowChicken = '#ffd72e';
-export const yellowShadow = '#ffbf0b';
+export const yellow = '#ffd72e';
+export const darkYellow = '#ffbf0b';
 export const orange = '#f36d21';
+
+// grayscale
+export const black = '#000000';
+export const darkgray = '#242423';
+export const gray = '#333533';
+export const lightgray = '#CFDBD5';
+export const platinum = '#E8EDDF';
+export const white = '#FFFFFF';
+
+// validations
+export const blue = '#5995ED';
+export const green = '#B9E28C';
+export const red = '#FB4D3D';
+
+// purplescale
+export const purpleGrape = '#5B507A';
+export const blueGrape = '#5B618A';
+export const lightBlueGrape = '#9EADC8';
+
+// bright colors
+export const pink = '#F00699';
