baa5c6120518ddf75cdb7135e560c632bad2b7e3
leire <leirericop@gmail.com>
2020-03-29 20:33:02 +0200

    feat(header): create header component and some common components

--- src/components/containers.js
+++ src/components/containers.js
@@ -3,11 +3,8 @@ import { device } from 'constants/device';
 
 export const Wrapper = styled.section`
   padding: 0.5em 1em;
+
   @media ${device.tablet} {
     padding: 1.5em 2em;
   }
-`;
-
-export const List = styled.ol`
-  list-style: none;
 `;
