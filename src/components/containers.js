0abae2643d58c037af03d07050bca559ec59bb1a
leire <leirericop@gmail.com>
2020-04-03 18:15:35 +0200

    fix(background): create background svg

--- src/components/containers.js
+++ src/components/containers.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 import { device } from 'constants/device';
+import background from 'assets/background.svg';
 
 export const Wrapper = styled.section`
+  background-image: url(${background});
+  background-repeat: repeat;
+  
+  height: 100vh;
   padding: 0.5em 1em;
 
   @media ${device.tablet} {
